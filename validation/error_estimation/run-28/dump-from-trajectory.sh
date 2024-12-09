6c9555696a9acc559920e20822b5b209f1daf22a
a.hz <79799631+abhigyanh@users.noreply.github.com>
2024-12-09 21:36:06 +0530

    add more validation data

---
+++ validation/error_estimation/run-28/dump-from-trajectory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# First use 'gmx trjconv' to make all molecules whole
+echo "System" | gmx trjconv -f traj.trr -s example.tpr -o traj_whole.trr -pbc whole 
+
+# Then use 'gmx traj' to dump coordinates and velocities in numpy-readable format.
+mkdir ./dodos
+gmx traj -f traj_whole.trr -s example.tpr -n not_name_MW.ndx -ov ./dodos/veloc.xvg -ox ./dodos/pos.xvg -ob ./dodos/box.xvg
+
+# Remove 'traj_whole.trr' because it's not needed beyond this point
+rm traj_whole.trr
